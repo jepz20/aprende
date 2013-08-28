986f85e39a86ddf95370843bbf8d064ab6414345
Jose Perdomo <jepz20@gmail.com>
2013-08-28 16:13:48 +0000

    se resolvio el problema de program euler 01

---
+++ pe001.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+//http://projecteuler.net/problem=1
+
+var divides = function(a,b) {
+    return b%a === 0;
+};
+
+var anydivide = function (as,b){
+    for(var ii in as ) {
+	if(divides(as[ii], b)) {
+	    return true;
+	}
+    }
+    return false;
+};
+
+var sum = function(arr){
+    var cc = 0;
+    for (var ii in arr){
+	cc += arr[ii];
+    }
+    return cc;
+};
+
+var fizzbuzz = function (factors,max) {
+    var out =[];
+    for (var nn = 1; nn < max; nn +=1){
+	if(anydivide(factors, nn)){
+	    out.push(nn);
+	}
+    }
+    return sum(out);
+};
+
+console.log(fizzbuzz([3,5],1000));
